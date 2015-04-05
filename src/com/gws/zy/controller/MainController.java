061df5160c196d795774add7cbbed64488f74b44
zhangwei900808 <zhangwei900808@hotmail.com>
2015-04-05 17:28:35 +0800

    添加和修改类文件和页面文件

---
+++ src/com/gws/zy/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.gws.zy.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MainController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {		
+		return new ModelAndView("main/index");
+	}
+}
